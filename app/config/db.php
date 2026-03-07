cebe18c154ee73575e4cc2a08bbb8546cba3e3eb
shakila735 <afrin2305341398@diu.edu.bd>
2026-03-07 13:30:49 +0600

    Add MySQL database schema for users and blood requests tables

---
+++ app/config/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$dbname = "blood_donor_system";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+?>
