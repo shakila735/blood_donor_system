17395fc1853f535710fbbac423f6b3773f129b42
shakila735 <afrin2305341398@diu.edu.bd>
2026-04-05 12:54:09 +0600

    style: setup foundation styling file and empty JS base structure

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+/* assets/js/script.js */
+// Any future common JavaScript logic can go here.
+// Currently primarily keeping form validation in register.php explicitly.
+console.log("BloodNet Phase 1 JS loaded.");
